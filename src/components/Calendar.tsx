02e01afee03d67ab5c49e80e607a7350bc2c7f3b
Artur <ohotnikarturik@gmail.com>
2024-04-12 22:36:06 +0300

    refactor: remove unnecessary fils

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,16 +1,22 @@
 "use client"
 
-import { LocalizationProvider } from "@mui/x-date-pickers"
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers"
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3"
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar"
 import { useState } from "react"
 
 export default function BasicDateCalendar() {
-  const [value, setValue] = useState(new Date())
+  const [value, setValue] = useState<Date>(new Date())
+
   console.log("value", value, new Date(value))
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+      <DatePicker
+        label="Date Picker"
+        format="dd.MM.yyyy"
+        onChange={(newValue) => {
+          setValue(newValue as Date)
+        }}
+      />
     </LocalizationProvider>
   )
 }
--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography"
 export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © ArtEnergy "}
+      {"Copyright © ArtDev"}
       {new Date().getFullYear()}
     </Typography>
   )
