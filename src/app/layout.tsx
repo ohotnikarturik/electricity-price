ddfe1ce07efc4675ceb4da3241ad35208f63cfe3
Artur <ohotnikarturik@gmail.com>
2024-04-14 10:37:00 +0300

    feat: remove force-dynamic option

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,6 @@ import { Box, Container } from "@mui/material"
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 
-export const dynamic = "force-dynamic"
-
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
