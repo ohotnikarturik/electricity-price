4e5d3b7d6abf772b069086f75fff851c664f3b35
Artur <ohotnikarturik@gmail.com>
2024-05-02 14:32:30 +0300

    refactor: remove api folder

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { Prices } from "nordpool"
-
-const prices = new Prices()
-
-export async function GET() {
-  const hourlyPrices = await prices.hourly({ area: "FI" })
-  return Response.json(hourlyPrices)
-}
